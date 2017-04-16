6bca7233d570bcdc7b1164338026f494879b97d4
Bozhin Zafirov <bozhin@sirius.local>
2017-04-16 13:55:22 +0300

    combine archive payload parsers in the one module

--- extmilter/archive.go
+++ extmilter/archive.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"archive/zip"
 	"github.com/nwaples/rardecode"
 	"io"
 	"path/filepath"
 	"strings"
 )
+
+/* AllowZipPayload inspecs a zip attachment in email message and
+   returns true if no filenames have a blacklisted extension */
+func AllowZipPayload(r *strings.Reader) (bool, error) {
+	reader, err := zip.NewReader(r, int64(r.Len()))
+	if err != nil {
+		return false, err
+	}
+
+	// range over filenames in zip archive
+	for _, f := range reader.File {
+		FileExt := filepath.Ext(strings.ToLower(f.Name))
+		if !AllowFilename(FileExt) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
 
 /* AllowRarPayload inspects a rar attachment in email message and
    returns true if no filenames have a blacklisted extension */
